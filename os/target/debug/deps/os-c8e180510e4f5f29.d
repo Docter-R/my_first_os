03d899dbaf10e9e89954e176d300803e7d307a0a
Docter-R <3226440754@qq.com>
2025-11-27 01:27:14 +0800

    no_std

---
+++ os/target/debug/deps/os-c8e180510e4f5f29.d
@@ -0,0 +1,5 @@
+/home/Zhang/rust_exercise/my_os/os/target/debug/deps/os-c8e180510e4f5f29.d: src/main.rs
+
+/home/Zhang/rust_exercise/my_os/os/target/debug/deps/os-c8e180510e4f5f29: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-95690e76b33de7fc.d
@@ -0,0 +1,5 @@
+/home/Zhang/rust_exercise/my_os/os/target/riscv64gc-unknown-none-elf/debug/deps/os-95690e76b33de7fc.d: src/main.rs
+
+/home/Zhang/rust_exercise/my_os/os/target/riscv64gc-unknown-none-elf/debug/deps/os-95690e76b33de7fc: src/main.rs
+
+src/main.rs:
